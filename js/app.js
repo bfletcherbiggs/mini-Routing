5e971069705aed11a1005d6df8a71cf039778d35
Benjamin Biggs <benjaminbiggs@MacBook-Pro.local>
2017-02-23 21:16:50 -0600

    done

---
+++ js/app.js
@@ -0,0 +1,26 @@
+var myApp = angular.module('miniRouting', ['ui.router'])
+
+myApp.config(function($stateProvider, $urlRouterProvider) {
+    $stateProvider
+        .state('home', {
+            url: '/',
+            templateUrl: 'js/home/homeTmpl.html',
+            controller: 'homeCtrl'
+        })
+        .state('settings', {
+            url: '/settings',
+            templateUrl: 'js/settings/settingsTmpl.html',
+            controller: 'settingsCtrl'
+        })
+        .state('products', {
+            url: '/products/:id',
+            templateUrl: 'js/products/productTmpl.html',
+            controller: 'productsCtrl'
+        });
+
+    $urlRouterProvider
+        .otherwise('/');
+
+
+
+})
